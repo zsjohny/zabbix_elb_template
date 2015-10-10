6ec0320a4ec14e844d02cf7c5c1595c0a012f429
johny.zheng <johny.zheng@ylly.com>
2015-10-10 16:26:38 +0800

    Update elb_stats.py

--- elb_stats.py
+++ elb_stats.py
@@ -13,7 +13,7 @@ parser.add_option("-a", "--access-key", dest="access_key",
 parser.add_option("-s", "--secret-key", dest="secret_key",
                 help="AWS Secret Access Key")
 parser.add_option("-m", "--metric", dest="metric",
-                help="RDS cloudwatch metric")
+                help="AWS cloudwatch metric")
 parser.add_option("-r", "--region", dest="region",
                 help="AWS region")
 
@@ -26,9 +26,7 @@ if (options.access_key == None):
 if (options.secret_key == None):
     parser.error("-s AWS Secret Key is required")
 if (options.metric == None):
-    parser.error("-m RDS cloudwatch metric is required")
-if (options.region == None):
-    parser.error("-r AWS region is required")    
+    parser.error("-m AWS cloudwatch metric is required")
 ###
 
 ### Real code
